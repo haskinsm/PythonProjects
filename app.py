1b4ba8fca5f6b761f89306a310ffa57a84c6a996
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-04-16 18:34:45 +0100

    Commit of early attempt at Script2

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ Created on Mon Apr 12 21:38:43 2021
 
 @author: micha
 """
+## When you run the program search http://127.0.0.1:8050/ and the 
+#  interactive dashboard should be visible and working
 
 import dash #  helps you initialize your application (dashboard)
 import dash_core_components as dcc # allows you to create interactive components like graphs, dropdowns, or date ranges
@@ -11,6 +13,9 @@ import dash_html_components as html # lets you access HTML tags
 import pandas as pd
 import numpy as np
 from dash.dependencies import Output, Input
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 ## Read in the 3 datasets. 
 datasetJan20 = pd.read_csv("Homelessness Report January 2020.csv")
@@ -39,7 +44,7 @@ df = df[cols]
 
 
 ## Now begin creating a Dash Dashboard by Plotly
-# First will link a CSS stylesheet 
+# First will link a CSS stylesheet located in my working directory 
 external_stylesheets = [
     {
         "href": "style.css"
@@ -52,7 +57,7 @@ external_stylesheets = [
 # the head tag of the application
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets) ## creates instance of the dash class
-server = app.server 
+# server = app.server # Tried releasing the app on Heroku but can't get it to work right on that platform
 app.title = "Homelessness Dashboard" ## This is the text that will appear in the title bar of your web browser 
 
 # Define the layout of the dash application
@@ -108,6 +113,12 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=dcc.Graph(
+                        id="Region-chart", config={"displayModeBar" : False},
+                    ),
+                    className="card"
+                ),
+                html.Div(
+                    children=dcc.Graph(
                         id="Adult-chart", config={"displayModeBar" : False},
                     ),
                     className="card",
@@ -140,7 +151,7 @@ app.layout = html.Div(
     # The indentifier element that they'll modify when the function executes
     # The property of the element to be modified. 
 @app.callback(
-    [Output("Adult-chart", "figure"), Output("Families-chart", "figure")],
+    [Output("Region-chart", "figure"), Output("Adult-chart", "figure"), Output("Families-chart", "figure")],
     [
         Input("region-filter", "value"),
         Input("date-range", "start_date"),
@@ -157,6 +168,31 @@ def update_charts(region, start_date, end_date):
         & (df.date <= end_date)
     )
     filtered_data = df.loc[mask, :]
+    
+    fig = make_subplots(rows=1, cols=3, subplot_titles=("", "Breakdown of Homeless Adults by Region", ""), specs=[[{"type": "pie"}, {"type": "pie"}, {"type": "pie"}]])
+    
+    fig.add_trace(go.Pie( 
+            values= df["Total Adults"].head(9), 
+            labels=df["Region"].head(9),
+            title ="January"
+        ),  
+        row=1, col=1)
+    fig.add_trace(go.Pie( 
+            values= df["Total Adults"].head(18).tail(9), 
+            labels=df["Region"].head(18).tail(9),
+            title ="June"
+        ),
+        row=1, col=2)
+    fig.add_trace(go.Pie( 
+            values=df["Total Adults"].tail(9), 
+            labels=df["Region"].tail(9),
+            title ="December"
+        ),
+        row=1, col=3)
+    
+    # region_chart_figure = px.pie(df.head(9), values="Total Adults", names="Region", title= "Regional Breakdown of homeless Adults")
+    region_chart_figure = fig
+     
     adult_chart_figure = {
         "data": [
             {
@@ -193,7 +229,7 @@ def update_charts(region, start_date, end_date):
             "colorway": ["#E12D39"],
         },
     }
-    return adult_chart_figure, family_chart_figure
+    return region_chart_figure, adult_chart_figure, family_chart_figure
 
 
 
