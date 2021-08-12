3b0e7497abf26a2b49dd49070cb179073fd8ca1a
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-08-12 17:13:55 +0100

    Update to repo structure

---
+++ scripts/Git_Commands.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 12:54:53 2021
+
+@author: micha
+"""
+# !git remote add origin https://github.com/haskinsm/PythonProjects.git
+# !git remote -v ##checks origin
+
+
+# !git add "addition.py" or !git add "scripts"
+# !git commit -m "2nd Commit"
+# !git push origin main
+
+# Forced:  !git push -f origin main
+
+# !git branch
+
+# !git fetch origin master
+# !git merge origin/other-branch
+# Fix merge conflicts, if they occur
+# Add merge conflict fixes 
+# !git commit    # And commit the merge!
+
+# !git branch -f remote_branch_name origin/remote_branch_name 
+# !git checkout remote_branch_name
+
+# !git switch <existing_branch>
