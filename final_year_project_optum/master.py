cb730647999b09221adab009bfc3efc4ef10965f
haskinsm <60662825+haskinsm@users.noreply.github.com>
2021-11-14 21:17:27 +0000

    Minor update to FYP master script

--- final_year_project_optum/master.py
+++ final_year_project_optum/master.py
@@ -5,15 +5,16 @@ Created on Mon Nov  1 15:02:43 2021
 @author: micha
 """
 import os
-
-# Change directory to correctDir or script wont run correctly
-correctDir = "C:\\Users\\micha\\Documents\\3rd year\\Software Applications\\PythonSpyder(Anaconda V)\\final_year_project_optum"
-os.chdir(correctDir)
-
-
-
 ##### Import scripts I've written
 import scripts_and_data
+
+# Change directory to correctDir or script wont run correctly
+CORRECTDIR = "C:\\Users\\micha\\Documents\\3rd year\\Software Applications\\PythonSpyder(Anaconda V)\\final_year_project_optum"
+os.chdir(CORRECTDIR)
+
+
+
+
 # Create shorter references for these classes
 rf = scripts_and_data.scripts.random_forest
 data = scripts_and_data.data.titanic.Titanic
