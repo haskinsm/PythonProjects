407bd0eac1105dce7acbf4749558f075f0679798
haskinsm <60662825+haskinsm@users.noreply.github.com>
2022-01-03 15:43:51 +0000

    Completed water_pump_dataset_manip.py. Started work on water_pump_dataset.py

--- final_year_project_optum/scripts_and_data/data/dataset_manipulation/water_pump_dataset_manip.py
+++ final_year_project_optum/scripts_and_data/data/dataset_manipulation/water_pump_dataset_manip.py
@@ -10,19 +10,23 @@ description: This is where the orginal water pump dataset is read in and data ma
 import pandas as pd 
 import numpy as np
 import os
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+#from fast_ml.model_development import train_valid_test_split
+from sklearn.compose import ColumnTransformer
+
 
 ############################ Read in data #################################
-#get the current working directory 
-currentDir = os.getcwd()
-## Check if in correct w. directory (C:\Users\micha\Documents\3rd year\Software Applications\PythonSpyder(Anaconda V)\final_year_project_optum)
-
+# Change to correct directory 
+correctDirPath = "C:/Users/micha/Documents/3rd year/Software Applications/PythonSpyder(Anaconda V)/final_year_project_optum"
+os.chdir(correctDirPath)
 
 # Append name of file you want to read to the current directory
-datasFilePath = os.path.join(currentDir, "scripts_and_data\\data\\datasets\\water_pump_data\\training_set.csv")
+datasFilePath = os.path.join(correctDirPath, "scripts_and_data\\data\\datasets\\water_pump_data\\training_set.csv")
 # read in data (in this case the labels could only be downloaded seperately)
 dataValues = pd.read_csv(datasFilePath, parse_dates = ['date_recorded'])  
 # read in labels
-labelsFilePath = os.path.join(currentDir, "scripts_and_data\\data\\datasets\\water_pump_data\\training_set_labels.csv")
+labelsFilePath = os.path.join(correctDirPath, "scripts_and_data\\data\\datasets\\water_pump_data\\training_set_labels.csv")
 dataLabels = pd.read_csv(labelsFilePath) # this contains the vars ID and the target variable status_group 
 
 
@@ -35,53 +39,62 @@ data = pd.merge(left = dataValues, right = dataLabels, left_on = 'id', right_on 
 data = data.set_index('id')
 
 # 59,000 rows, 40 columns 
-data.info()
-data.describe()
+#data.info()
+#data.describe()
 
-########################## Deal with missing data ######################################
+########################## Droping variables ###################################
 # Check for missing data
-data.isnull().sum()
+#data.isnull().sum()
 # drop the column scheme name as too many missing values and not useful
 data.drop('scheme_name', axis = 1, inplace = True)
 
-# give nan values the value of zero 
-data['funder'] = data['funder'].fillna(0)
-data['installer'] = data['installer'].fillna(0)
-data['subvillage'] = data['subvillage'].fillna(0)
-data['public_meeting'] = data['installer'].fillna(0)
-data['scheme_management'] = data['scheme_management'].fillna(0)
-data['permit'] = data['permit'].fillna(0)
+##### Remove duplicate and very similar variables
+data.drop('payment', axis = 1, inplace = True) # Duplicate of payment_type
+data.drop('quantity_group', axis = 1, inplace = True) # Duplicate of quantity
+data.drop('extraction_type', axis = 1, inplace = True) # Duplicate of extraction_type_group
+
+# The variable recorded_by only contains one value, so it is useless information 
+data.drop('recorded_by', axis = 1, inplace = True)
+
+#### Get number of levels of variable
+# Cant have variables with large number of unique values as my laptop is not able to handle OneHotEncoding these vars
+levels = {}
+for i in data.columns:
+    levels[i] = data[i].value_counts()
+#print(levels)
+data.drop('wpt_name', axis = 1, inplace = True) ##37,400 unique values -> this is too many so drop this variable 
+data.drop('subvillage', axis = 1, inplace = True) #19,288 unique values
+data.drop('num_private', axis = 1, inplace = True) # 98.7% are 0
+data.drop('public_meeting', axis = 1, inplace = True) #2146 unique values
+
+########################## Deal with missing data ######################################
+# give nan values sensible values
+data['funder'] = data['funder'].fillna('unknown') #unknown
+data['installer'] = data['installer'].fillna('unknown') #unknown
+data['scheme_management'] = data['scheme_management'].fillna('unknown') #unknown
+data['permit'] = data['permit'].fillna(data['permit'].median()) #median
 
 ### Other instances of missing data
-## The variable wpt_name has observations with value of 'none', but this is accpetable 
 ## The variable population has a number of observations with the value of zero. It is unclear whether this 
-   # is missing data or is real data **************************************************** Ask Peter 
+   # is missing data or is real data 
 ## The variable construction_year contains missing data with the value of zero. The first 'real' recording of construction year is 
    # 1960. So I will reset construction year so that 1960 has value of 1, 1961 has value of 2, etc and the missing data
    # has the median value of 27 
-temp = data
 data['construction_year'].replace(0, data['construction_year'].median(), inplace = True) 
 data['construction_year'] = data['construction_year'] - 1959
 # now construction year has missing data with median value of 27 and all the other datapoints values range from 1-> 54
 
 # Many of the variables contain the value 'unknown', but this is acceptable 
 
-# The variables payment & payment_type, quantity & quantity_group, waterpoint_type & waterpoint_type_group
-# appear to be displaying the same information so drop one variable in each pair 
-data.drop('payment', axis = 1, inplace = True)
-
-   
-
-
 ######################## Further data manipultaion ############################
-# The variable recorded_by only contains one value, so it is useless information 
-data.drop('recorded_by', axis = 1, inplace = True)
 
 # The target variable contrains 3 levels:P functional, non-functional and needs repair 
 # I will create another variable called repair which will indicate if a water pump needs to be repaired
 # 0 will indicate no repair needed, 1 will indicate repair needed. I will give non-functional water pumps 
 # a repair value of 1 
 ## Think I may have made this too influential on the outcome so may need to be changed*****************************************
+"""
+### Will prob remove this section 
 def repair_labels(row):
     if row['status_group'] == 'functional':
         return 0
@@ -91,6 +104,7 @@ def repair_labels(row):
         return 1
     return 'Other'
 data['repair'] = data.apply(lambda row: repair_labels(row), axis = 1)
+"""
 
 def response_labels(row):
     if row['status_group'] == 'non functional':
@@ -99,22 +113,77 @@ def response_labels(row):
         return 1
 data['status_group'] = data.apply(lambda row: response_labels(row), axis = 1)
 
+"""
+### Remove this if remove repair var 
 # Switch position of columns repair and status_group so status_group is last (it is the response variable) 
 colList = list(data)
-colList[37], colList[38] = colList[38], colList[37]
+colLength = len(colList)
+colList[colLength - 2], colList[colLength - 1] = colList[colLength - 1], colList[colLength - 2]
 data.columns = colList
+"""
 
-# Get number of levels of variable
-len(data['wpt_name'].unique()) #37,400 -> this is too many so drop this variable 
-data.drop('wpt_name', axis = 1, inplace = True)
+    
 
-######################### Convert categorical data to integer levels ####################
-#newData, origLevels = pd.factorize(data[''])
-# OrdinalEncoder
-
-
+######## The location variables
+# There are variables recording the longitude and latitude, these variables will likely provide enough information to 
+# account for location of a water pump and so the variables district_code, region, region_code, subvillage and ward may 
+# not be useful, but for now I will leave them in
 
 
+######################### Convert categorical variables to be ordinal using OrdinalEncoder or nominal using OneHotEncoder ####################
+#### using OneHotEncoder for Nominal data and OrdinalEncoddr for ordianl data
+
+# While the variable quantity appears to be semi ordinal there is not enough information to warrent encoding
+# it as ordinal as it is unclear what order to put 'seasonal', 'unknown', 'dry', 'enough' , 'insufficient' in
+
+XNom = data.iloc[:, [2,4,7,8,9,10,11,12,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29]].astype(str)
+XCont = data.iloc[:, [0,1,3,5,6,13,16]].astype(str)
+y = pd.DataFrame(data.iloc[:, -1].astype(str))
+#onehotEnc = preprocessing.OneHotEncoder()
+#C = pd.DataFrame(onehotEnc.fit_transform(C).toarray()) # No column names 
+
+XNomDumDf = pd.get_dummies(XNom, columns = XNom.columns)
+# Now merge everything back together to get the final encoded dataset
+encData = pd.concat([pd.concat([XCont, XNomDumDf], axis = 1), y], axis = 1) 
+
+
+#################### Now save as a csv file ####################
+# This csv file will be read in in water_pump_dataset.py 
+csvFilePath = os.path.join(correctDirPath, "scripts_and_data\\data\\datasets\\water_pump_data\\processed_data.csv")
+encData.to_csv(csvFilePath, header = True, index = True)
+
+
+
+
+"""
+    ###### Discarded code 
+# First split into training set and then will split remaining terms again to get the test and validation sets
+XTrain, XRem, yTrain, yRem = train_test_split(X,y, train_size=0.6)
+XValid, XTest, yValid, yTest = train_test_split(XRem,yRem, test_size=0.5) #Test = 20% and validation = 20%
+
+# Label Encoder
+lblEnc = preprocessing.LabelEncoder()
+lblEnc.fit(yTrain)
+yTrain = lblEnc.transform(yTrain)
+yTest = lblEnc.transform(yTest)
+yValid = lblEnc.transform(yValid)
+
+# Use ColumnTransformer to transform specific nominal columns using OneHotEncoder 
+colTrans = ColumnTransformer([("categ", preprocessing.OneHotEncoder(), [2,4,7,8,9,10,11,12,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29] )])
+colTrans.fit(XTrain.toarray())
+XTrain = pd.DataFrame(colTrans.transform(XTrain.toarray()))
+XTest = colTrans.transform(XTest)
+XValid = colTrans.transform(XValid)
+
+
+
+# Onehot Encode nominal variables ALL ATM*****************
+onehotEnc = preprocessing.OneHotEncoder()
+onehotEnc.fit(X)
+encDf = pd.DataFrame(onehotEnc.transform(X).toarray())
+a = onehotEnc.transform(X)
+
+"""
 
 
 
--- final_year_project_optum/scripts_and_data/data/water_pump_dataset.py
+++ final_year_project_optum/scripts_and_data/data/water_pump_dataset.py
@@ -4,7 +4,24 @@ Created on Sat Dec 18 13:28:41 2021
 
 @author: micha
 
-description: This where the dataset will be read in and split up into training, test and validation sets. 
-            The data has already been prepped for use in machine learning algortihms in water_pump_dataset_manip 
+description: This where the already saved processed dataset will be read in and split up into training, test and validation sets. 
+            The data has already been prepped for use in machine learning algortihms in water_pump_dataset_manip and has been saved as
+            processedData.csv in data/datasets/water_pump_data/processed_data.csv
 """
 
+import pandas as pd 
+import numpy as np
+import re
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class WaterPump():
+    ############################ Read in data #################################
+    #get the current working directory 
+    currentDir = os.getcwd()
+   
+    # Append name of file to be read to the current directory 
+    testFilesDir = os.path.join(currentDir, "scripts_and_data\\data\\datasets\\water_pump_data\\processed_data.csv")
+    test = pd.read_csv(testFilesDir)
+    
